f5a4dc8cc70e9b57e5f8fa10a7ec9918097d1aee
bjb568 <bjb568@users.noreply.github.com>
2016-07-05 13:46:56 +0800

    Init New Question Set page

--- http/console.js
+++ http/console.js
@@ -1,1 +1,5 @@
 'use strict';
+document.getElementById('new-qset-btn').addEventListener('click', function() {
+	document.getElementById('new-qset').classList.remove('hide');
+	document.getElementById('qset-title').focus();
+});
