7faaaf6fd2bf7fcc19751ee1206698ea5f799f03
bjb568 <bjb568@users.noreply.github.com>
2016-07-15 15:47:12 +0800

    Init google signin

--- http/landing.js
+++ http/landing.js
@@ -1,17 +1,17 @@
 'use strict';
-function smoothScroll(el, t, p, s) {
-	p = t - p;
-	var dist = el.getBoundingClientRect().top,
-		now = new Date().getTime();
-	s = s || now;
-	var elapsed = now - s;
-	if (dist > 6 && document.body.scrollTop - document.body.scrollHeight + innerHeight) {
-		scrollBy(0, Math.min(dist - 5, Math.max(1, p * dist * elapsed * elapsed / 50000000)));
-		requestAnimationFrame(function(p) {
-			smoothScroll(el, p, t, s);
-		});
-	}
-}
-document.getElementById('scroll').addEventListener('click', function() {
-	smoothScroll(document.getElementById('benefits'));
+var user;
+gapi.load('auth2', function() {
+	gapi.auth2.init({
+		'client_id': '352625054764-n7d1ihmu0krk7tpc8mooolm3cueptqds.apps.googleusercontent.com',
+		'scope': 'profile',
+		'fetch_basic_profile': false
+	}).attachClickHandler(
+		document.getElementById('signin'), {},
+		function(returnedUser) {
+			user = returnedUser;
+		},
+		function(error) {
+			alert(JSON.stringify(error));
+		}
+	);
 });
