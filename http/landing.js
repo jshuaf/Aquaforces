62af5685e148feae518eb3f3ddaff9f5521b6f42
Joshua <joshuacfang@gmail.com>
2016-07-21 09:12:15 -0400

    added basic auth to get a unique ID

--- http/landing.js
+++ http/landing.js
@@ -1,18 +1,43 @@
 'use strict';
 var user;
-gapi.load('auth2', function() {
+const CLIENT_ID = '891213696392-0aliq8ihim1nrfv67i787cg82paftg26.apps.googleusercontent.com';
+
+gapi.load('auth2', () => {
 	gapi.auth2.init({
-		'client_id': '352625054764-n7d1ihmu0krk7tpc8mooolm3cueptqds.apps.googleusercontent.com',
+		'client_id': CLIENT_ID,
 		'scope': 'profile',
 		'fetch_basic_profile': false
-	}).attachClickHandler(
-		document.getElementById('log-in'), {},
-		function(returnedUser) {
-			user = returnedUser;
-			console.log(user);
-		},
-		function(error) {
-			alert(JSON.stringify(error));
-		}
-	);
+	});
+	document.getElementById('log-in').addEventListener('click', authorizeUser);
 });
+
+function httpGetAsync(url, callback) {
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.onreadystatechange = function() {
+		if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+			return callback(xmlHttp.responseText);
+		} else {
+			return false;
+		}
+	};
+	xmlHttp.open("GET", url, true);
+	xmlHttp.send(null);
+}
+
+function authorizeUser() {
+	const auth = gapi.auth2.getAuthInstance();
+	auth.signIn().then(() => {
+		const authResponse = auth.currentUser.get().getAuthResponse();
+		const idToken = authResponse.id_token;
+		const tokenVerificationURL = "https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=" + idToken;
+		httpGetAsync(tokenVerificationURL, (responseText) => {
+			const responseObject = JSON.parse(responseText);
+			if (responseObject.aud == CLIENT_ID) {
+				verifyID(reponseObject.sub);
+			}
+			else {
+				alert("ID Token integrity compromised.");
+			}
+		});
+	});
+}
