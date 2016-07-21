bf0a448f03dd8a9954d59639108f886754008b98
Joshua <joshuacfang@gmail.com>
2016-07-21 15:39:41 -0400

    got login working

--- http/a/main.js
+++ http/a/main.js
@@ -44,6 +44,16 @@ function requestGet(url, callback) {
 	xmlHttp.send(null);
 }
 
+const CLIENT_ID = '891213696392-0aliq8ihim1nrfv67i787cg82paftg26.apps.googleusercontent.com';
+
+gapi.load('auth2', () => {
+	gapi.auth2.init({
+		'client_id': CLIENT_ID,
+		'scope': 'profile',
+		'fetch_basic_profile': false
+	});
+});
+
 function authorizeUser() {
 	const auth = gapi.auth2.getAuthInstance();
 	auth.signIn().then(() => {
--- http/landing.js
+++ http/landing.js
@@ -1,12 +1,2 @@
 'use strict';
-var user;
-const CLIENT_ID = '891213696392-0aliq8ihim1nrfv67i787cg82paftg26.apps.googleusercontent.com';
-
-gapi.load('auth2', () => {
-	gapi.auth2.init({
-		'client_id': CLIENT_ID,
-		'scope': 'profile',
-		'fetch_basic_profile': false
-	});
-	document.getElementById('log-in').addEventListener('click', authorizeUser);
-});
+document.getElementById('log-in').addEventListener('click', authorizeUser);
