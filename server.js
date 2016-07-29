aef50aef6b1943d59a9fa4245fe59d42ed249427
bjb568 <bjb568@users.noreply.github.com>
2016-07-29 12:25:47 +0800

    Implement Google OAuth, fix #166, fix #146

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 'use strict';
+const fs = require('fs');
 /*eslint-disable no-process-env*/
 const config = {
 	port: process.env.PORT || (process.argv.includes('--production') ? 80 : 3000),
-	mongoPath: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/Aquaforces'
+	mongoPath: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/Aquaforces',
+	secureCookies: false,
+	googleAuth: {
+		'client_id': process.argv.includes('--test') ? '' : process.env.GITHUB_CLIENT_ID || fs.readFileSync('./github.pem').toString().split('\n')[0],
+		'client_secret': process.argv.includes('--test') ? '' : process.env.GITHUB_CLIENT_SECRET || fs.readFileSync('./github.pem').toString().split('\n')[1]
+	}
 };
 /*eslint-enable no-process-env*/
 require('./essentials.js');
@@ -11,10 +17,10 @@ global.dbcs = {};
 const usedDBCs = ['qsets', 'users'];
 
 const http = require('http'),
+	https = require('https'),
 	uglifyJS = require('uglify-js'),
 	CleanCSS = require('clean-css'),
 	zlib = require('zlib'),
-	fs = require('fs'),
 	path = require('path'),
 	url = require('url'),
 	querystring = require('querystring'),
@@ -56,6 +62,15 @@ global.respondPage = o(function*(title, req, res, callback, header, status) {
 	res.write(yield addVersionNonces(data.replace('xml:lang="en"', noBG ? 'xml:lang="en" class="no-bg"' : 'xml:lang="en"').replace('$title', (title ? title + ' · ' : '') + 'Aquaforces').replace('$inhead', inhead), req.url.pathname, yield));
 	callback();
 });
+global.errorsHTML = function(errs) {
+	return errs.length ?
+		(
+			errs.length == 1 ?
+				'<div class="error">' + errs[0] + '</div>'
+				: '<div class="error">\t<ul>\t\t<li>' + errs.join('</li>\t\t<li>') + '</li>\t</ul></div>'
+		)
+		: '';
+};
 let cache = {};
 const redirectURLs = ['/host', '/play', '/console'];
 let serverHandler = o(function*(req, res) {
@@ -154,12 +169,23 @@ let serverHandler = o(function*(req, res) {
 			});
 			res.end(cache[req.url.pathname][raw ? 'raw' : 'gzip']);
 		}
+	} else if (req.url.pathname == '/') {
+		yield respondPage(null, req, res, yield, {inhead: '<link rel="stylesheet" href="/landing.css" />'});
+		res.write((yield fs.readFile('./html/landing.html', yield)).toString().replace('$googleClientID', config.googleAuth.client_id));
+		res.end(yield fs.readFile('./html/a/foot.html', yield));
 	} else if (req.url.pathname == '/host/') {
+		let user = yield dbcs.users.findOne({
+			cookie: {
+				$elemMatch: {
+					token: cookie.parse(req.headers.cookie || '').id || 'nomatch',
+					created: {$gt: new Date() - 2592000000}
+				}
+			}
+		}, yield);
 		yield respondPage('Question Sets', req, res, yield, {inhead: '<link rel="stylesheet" href="/host.css" />', noBG: true});
 		let qsetstr = '';
 		const requestCookies = req.headers.cookie || '';
-		const userID = cookie.parse(requestCookies).userID;
-		dbcs.qsets.find({author: userID}).sort({timeAdded: -1}).each(o(function*(err, qset) {
+		dbcs.qsets.find({$or: [{userID: user._id}, {public: true}]}).sort({timeAdded: -1}).each(o(function*(err, qset) {
 			if (err) throw err;
 			if (qset) {
 				qsetstr += '<details class="qset" id="qset-' + qset._id + '"><summary><h2>' + html(qset.title) + '</h2> <small><a class="play">▶Play</a> <a class="dup">Duplicate</a> <a class="delete" title="delete">✕</a> <a href="#qset-' + qset._id + '" title="permalink">#</a></small></summary><ol>';
@@ -183,9 +209,130 @@ let serverHandler = o(function*(req, res) {
 				});
 				qsetstr += '</ol><a class="new-question">add question</a></details>';
 			} else {
-				res.write((yield fs.readFile('./html/host.html', yield)).toString().replace('$qsets', qsetstr));
+				let data = (yield fs.readFile('./html/host.html', yield)).toString().replace('$qsets', qsetstr).replace('$googleClientID', config.googleAuth.client_id);
+				if (user) data = data.replace(/<a.+?<\/a>/, 'Logged in as ' + user.name);
+				res.write(data);
 				res.end(yield fs.readFile('./html/a/foot.html', yield));
 			}
+		}));
+	} else if (req.url.pathname == '/login/google') {
+		let tryagain = '<a href="https://accounts.google.com/o/oauth2/v2/auth?client_id=' + config.googleAuth.client_id + '&amp;response_type=code&amp;scope=openid%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fplus.me&amp;redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Flogin%2Fgoogle">Try again.</a>';
+		if (req.url.query.error) {
+			yield respondPage('Login Error', req, res, yield, {}, 400);
+			res.write('<h1>Login Error</h1>');
+			res.write('<p>An error was received from Google. ' + tryagain + '</p>');
+			res.write(errorsHTML(['Error: ' + req.url.query.error]));
+			return res.end(yield fs.readFile('html/a/foot.html', yield));
+		}
+		if (!req.url.query.code) {
+			yield respondPage('Login Error', req, res, yield, {}, 400);
+			res.write('<h1>Login Error</h1>');
+			res.write('<p>No authentication code was received. ' + tryagain + '</p>');
+			return res.end(yield fs.readFile('html/a/foot.html', yield));
+		}
+		let googReq = https.request({
+			hostname: 'accounts.google.com',
+			path: '/o/oauth2/token',
+			method: 'POST',
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/x-www-form-urlencoded'
+			}
+		}, o(function*(googRes) {
+			let data = '';
+			googRes.on('data', function(d) {
+				data += d;
+			});
+			yield googRes.on('end', yield);
+			console.log(data);
+			try {
+				data = JSON.parse(data);
+			} catch (e) {
+				yield respondPage('Login Error', req, res, yield, {}, 500);
+				res.write('<h1>Login Error</h1>');
+				res.write('<p>An invalid response was received from Google. ' + tryagain + '</p>');
+				res.end(yield fs.readFile('html/a/foot.html', yield));
+			}
+			if (data.error) {
+				yield respondPage('Login Error', req, res, yield, {}, 500);
+				res.write('<h1>Login Error</h1>');
+				res.write('<p>An error was received from Google. ' + tryagain + '</p>');
+				res.write(errorsHTML([data.error + ': ' + data.error_description]));
+				return res.end(yield fs.readFile('html/a/foot.html', yield));
+			}
+			console.log('/plus/v1/people/me?key=' + encodeURIComponent(data.access_token));
+			let apiReq = https.get({
+				hostname: 'www.googleapis.com',
+				path: '/plus/v1/people/me?access_token=' + encodeURIComponent(data.access_token)
+			}, o(function*(apiRes) {
+				let apiData = '';
+				apiRes.on('data', function(d) {
+					apiData += d;
+				});
+				yield apiRes.on('end', yield);
+				try {
+					apiData = JSON.parse(apiData);
+				} catch (e) {
+					yield respondPage('Login Error', user, req, res, yield, {}, 500);
+					res.write('<h1>Login Error</h1>');
+					res.write('<p>An invalid response was recieved from the Google API. ' + tryagain + '</p>');
+					res.end(yield fs.readFile('html/a/foot.html', yield));
+				}
+				if (apiData.error) {
+					yield respondPage('Login Error', user, req, res, yield, {}, 500);
+					res.write('<h1>Login Error</h1>');
+					res.write('<p>An error was recieved from the Google API. ' + tryagain + '</p>');
+					res.write(errorsHTML([apiData.error + ': ' + apiData.error_description]));
+					return res.end(yield fs.readFile('html/a/foot.html', yield));
+				}
+				console.log(apiData);
+				let matchUser = yield dbcs.users.findOne({googleID: apiData.id}, yield),
+					idToken = crypto.randomBytes(128).toString('base64');
+				if (matchUser) {
+					dbcs.users.update({googleID: apiData.id}, {
+						$push: {
+							cookie: {
+								token: idToken,
+								created: new Date().getTime()
+							}
+						},
+						$set: {googleName: apiData.login}
+					});
+				} else {
+					dbcs.users.insert({
+						_id: generateID(),
+						cookie: [{
+							token: idToken,
+							created: new Date().getTime()
+						}],
+						googleID: apiData.id,
+						name: apiData.displayName
+					});
+				}
+				res.writeHead(303, {
+					Location: '/host/',
+					'Set-Cookie': cookie.serialize('id', idToken, {
+						path: '/',
+						expires: new Date(new Date().setDate(new Date().getDate() + 30)),
+						httpOnly: true,
+						secure: config.secureCookies
+					})
+				});
+				res.end();
+			}));
+			apiReq.on('error', o(function*(e) {
+				yield respondPage('Login Error', user, req, res, yield, {}, 500);
+				res.write('<h1>Login Error</h1>');
+				res.write('<p>HTTP error when connecting to the Google API: ' + e + ' ' + tryagain + '</p>');
+				res.end(yield fs.readFile('html/a/foot.html', yield));
+			}));
+		}));
+		googReq.end('client_id=' + config.googleAuth.client_id + '&client_secret=' + config.googleAuth.client_secret + '&code=' + encodeURIComponent(req.url.query.code) + '&redirect_uri=' + encodeURIComponent('http://' + req.headers.host + '/login/google') + '&grant_type=authorization_code');
+		googReq.on('error', o(function*(e) {
+			yield respondPage('Login Error', req, res, yield, {}, 500);
+			res.write('<h1>Login Error</h1>');
+			res.write('<p>HTTP error when connecting to Google: ' + e + ' ' + tryagain + '</p>');
+			res.end(yield fs.readFile('html/a/foot.html', yield));
 		}));
 	} else if (redirectURLs.includes(req.url.pathname)) {
 		res.writeHead(303, {'Location': req.url.pathname + '/'});
@@ -206,6 +353,7 @@ mongo.connect(config.mongoPath, function(err, db) {
 	console.log(('Aquaforces running on port ' + config.port + ' over plain HTTP.').cyan);
 	require('./sockets.js')(server);
 	console.log(('Sockets running on port ' + config.port + ' over plain WS.').cyan);
+	dbcs.qsets.update({}, {$set: {public: true}}, {multi: true});
 	if (process.argv.includes('--test')) {
 		console.log('Running test, process will terminate when finished.'.yellow);
 		http.get({
