b11bfadf9a8cde522b6f480b1b65f2bdfc06415a
bjb568 <bjb568@users.noreply.github.com>
2016-08-04 12:14:59 +0800

    Add status page

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const usedDBCs = ['users', 'gameplays'];
 
 const http = require('http'),
 	https = require('https'),
+	spawn = require('child_process').spawn,
 	uglifyJS = require('uglify-js'),
 	CleanCSS = require('clean-css'),
 	zlib = require('zlib'),
@@ -361,6 +362,23 @@ let serverHandler = o(function*(req, res) {
 			res.write('<h1>Aquaforces play statistics</h1>');
 			res.write('<p>' + result[0].sum + ' users</p>');
 			res.write('<p>' + result[0].num + ' gameplays</p>');
+			res.end(yield fs.readFile('html/a/foot.html', yield));
+		}));
+	} else if (req.url.pathname == '/status/') {
+		yield respondPage('Status', req, res, yield);
+		res.write('<h1>Aquaforces Status</h1>');
+		let child = spawn('git', ['rev-parse', '--short', 'HEAD']);
+		res.write('<p class="green"><strong>Running</strong>, commit #');
+		child.stdout.on('data', function(data) {
+			res.write(data);
+		});
+		child.stdout.on('end', o(function*() {
+			res.write('</p>');
+			if (user.name) res.write('<p>You are logged in as <strong>' + user.name + '</strong></p>');
+			else res.write('<p>You are not logged in</p>');
+			res.write('<p>Current host header is <strong>' + req.headers.host + '</strong></p>');
+			res.write('<code class="blk" id="socket-test">Connecting to socket…</code>');
+			res.write(yield addVersionNonces('<script src="/a/sockettest.js"></script>', req.url.pathname, yield));
 			res.end(yield fs.readFile('html/a/foot.html', yield));
 		}));
 	} else if (redirectURLs.includes(req.url.pathname)) {
