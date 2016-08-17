6c255dbb4c92d9dd8a66e846dabc31aea1cfbda6
Joshua <joshuacfang@gmail.com>
2016-08-17 17:03:15 -0400

    add the rest of the reducers

--- http/host/reducers.js
+++ http/host/reducers.js
@@ -35,6 +35,54 @@ function newQuestionSet(state = initialQuestionSetState, action) {
 				return question;
 			}),
 		});
+	case actions.EDIT_CORRECT_ANSWER:
+		return Object.assign({}, state, {
+			questions: state.questions.map((question) => {
+				if (question.id === action.questionID) {
+					return Object.assign({}, question, {
+						correctAnswer: action.text,
+					});
+				}
+				return question;
+			}),
+		});
+	case actions.EDIT_INCORRECT_ANSWER:
+		return Object.assign({}, state, {
+			questions: state.questions.map((question) => {
+				if (question.id === action.questionID) {
+					return Object.assign({}, question, {
+						incorrectAnswers: question.incorrectAnswers.map((incorrectAnswer) => {
+							if (incorrectAnswer.id === action.answerID) {
+								return Object.assign({}, incorrectAnswer, {
+									text: action.text,
+								});
+							}
+							return incorrectAnswer;
+						}),
+					});
+				}
+				return question;
+			}),
+		});
+	case actions.EDIT_QUESTION_TEXT:
+		return Object.assign({}, state, {
+			questions: state.questions.map((question) => {
+				if (question.id === action.questionID) {
+					return Object.assign({}, question, {
+						text: action.text,
+					});
+				}
+				return question;
+			}),
+		});
+	case actions.EDIT_SET_TITLE:
+		return Object.assign({}, state, {
+			title: action.text,
+		});
+	case actions.TOGGLE_SET_PRIVACY:
+		return Object.assign({}, state, {
+			privacy: action.private,
+		});
 	default:
 		return state;
 	}
