2547a082ba79354f705d944ccc33b29f10175786
Joshua <joshuacfang@gmail.com>
2016-08-22 16:43:40 -0400

    fix rock prop errors

--- http/game/play/Rock.jsx
+++ http/game/play/Rock.jsx
@@ -1,23 +1,25 @@
 import React, { Component, PropTypes } from 'react';
 
-function Rock({ y, present }) {
-	const rockStyle = {
-		height: '100%',
-	};
-	let containerStyle = {
-		textAlign: 'center',
-		height: '12%',
-		margin: '0 auto',
-		transform: `translate(0px, ${y}px)`,
-		display: 'table',
-	};
-	if (!present) containerStyle.display = 'none';
-	return (
-		<div style={containerStyle}>
-			<img src="../img/obstacles/rock.svg" style={rockStyle} />
-		</div>
-	);
-	// rock slowly approaches
+class Rock extends Component {
+	render() {
+		const rockStyle = {
+			height: '100%',
+		};
+		let containerStyle = {
+			textAlign: 'center',
+			height: '12%',
+			margin: '0 auto',
+			transform: `translate(0px, ${this.props.y}px)`,
+			display: 'table',
+		};
+		if (!this.props.present) containerStyle.display = 'none';
+		return (
+			<div style={containerStyle}>
+				<img src="../img/obstacles/rock.svg" style={rockStyle} />
+			</div>
+		);
+		// rock slowly approaches
+	}
 }
 
 export default Rock;
