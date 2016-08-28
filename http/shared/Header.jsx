6b22bb0ba81925970ee50593176a0090f3378099
Joshua <joshuacfang@gmail.com>
2016-08-28 15:06:33 -0400

    make header hover work and add link to landing

--- http/shared/Header.jsx
+++ http/shared/Header.jsx
@@ -1,7 +1,8 @@
 import React, { Component, PropTypes } from 'react';
+import Radium from 'radium';
 import colors from '../shared/colors';
 
-export function Header() {
+function Header() {
 	const containerStyle = {
 		display: 'flex',
 		alignItems: 'center',
@@ -33,35 +34,34 @@ export function Header() {
 		alignItems: 'center',
 		textDecoration: 'none',
 		fontSize: '1.4em',
+		color: colors.midnight,
+		':hover': {
+			color: colors.pacific,
+		},
 	};
 
 	const linkTextStyle = {
-		color: colors.midnight,
+		color: 'inherit',
 	};
 
 	return (
 		<div style={containerStyle}>
-			<div style={logoContainerStyle}>
+			<a href="/" style={logoContainerStyle}>
 				<img src="../img/logo-black.svg" alt="Aquaforces" style={logoStyle} />
-			</div>
-			<a href="/console" style={linkStyle}><span style={linkTextStyle}>Question Sets</span></a>
-			<a href="/host" style={linkStyle}><span style={linkTextStyle}>Start a game</span></a>
-			<a href="/play" style={linkStyle}><span style={linkTextStyle}>Join a game</span></a>
+			</a>
+			<a href="/console" style={linkStyle} key={0}><span style={linkTextStyle}>Question Sets</span></a>
+			<a href="/host" style={linkStyle} key={1}><span style={linkTextStyle}>Start a game</span></a>
+			<a href="/play" style={linkStyle} key={2}><span style={linkTextStyle}>Join a game</span></a>
 		</div>
 	);
 }
 
-export function UnderHeader({ children }) {
+function UnderHeader() {
 	const style = {
 		height: '15vh',
 		width: '100%',
 	};
-
-	return (
-		<div style={style} />
-	);
+	return <div style={style} />;
 }
 
-UnderHeader.propTypes = {
-	children: PropTypes.any,
-};
+module.exports = { Header: new Radium(Header), UnderHeader };
