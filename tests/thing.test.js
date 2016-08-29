04381bf8a534778ad2b4740afa0f571588a49094
Joshua <joshuacfang@gmail.com>
2016-08-29 12:07:12 -0400

    trying to get tests running

---
+++ tests/thing.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import PrimaryButton from '../http/shared/PrimaryButton.jsx';
+
+describe('PrimaryButton', () => {
+	it('changes the color when hovered', () => {
+		const component = renderer.create(
+			<PrimaryButton>Click me</PrimaryButton>
+		);
+		let tree = component.toJSON();
+		expect(tree).toMatchSnapshot();
+
+		tree.props.onMouseEnter();
+		tree = component.toJSON();
+		expect(tree).toMatchSnapshot();
+
+		tree.props.onMouseLeave();
+		tree = component.toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+});
