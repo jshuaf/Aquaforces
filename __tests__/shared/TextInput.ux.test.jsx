13d12e984362ea18e7c68bcbf8e12cb3f9d8b5e0
Joshua <joshuacfang@gmail.com>
2016-08-30 12:27:03 -0400

    add more tests for textinput

--- __tests__/shared/TextInput.ux.test.jsx
+++ __tests__/shared/TextInput.ux.test.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import TextInput from '../../http/shared/TextInput.jsx';
 
 describe('TextInput', () => {
 	it('calls onComplete when its content is full', () => {
 		const onComplete = jest.fn();
 		const isComplete = text => text.length > 5;
-		const wrapper = mount(<TextInput onComplete={onComplete} isComplete={isComplete} />);
+		const wrapper = mount(<TextInput onComplete={onComplete} isComplete={isComplete} label="" />);
+		wrapper.instance().node.value = 'Hello';
 		wrapper.find('input').simulate('change', { target: { value: 'Hello' } });
 		expect(onComplete).not.toBeCalled();
+		wrapper.instance().node.value = 'Hello?';
 		wrapper.find('input').simulate('change', { target: { value: 'Hello?' } });
 		expect(onComplete).toBeCalled();
 	});
