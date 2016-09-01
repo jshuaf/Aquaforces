51eea279c72233167d363103e1fe0af2822b58eb
Joshua <joshuacfang@gmail.com>
2016-09-01 07:06:55 -0400

    fix client side request destroying

--- http/console/thunks.js
+++ http/console/thunks.js
@@ -8,21 +8,13 @@ const searchRequests = [];
 export function searchQuestionSets(query) {
 	return (dispatch, getState) => {
 		const url = `${location.protocol}//${location.host}/api/search`;
+		const body = { query };
 		if (!query) return;
-		/* if (getState().requests.search) {
-			getState().requests.search.forEach(req => req.abort());
-		}*/
-		/* dispatch(actions.clearSearchRequests());
-		searchRequests.forEach((request) => {
-			console.log(request);
-			request.abort();
-		});*/
-		const searchRequest = request({
-			url,
-			body: { query },
-			json: true,
-			method: 'post',
-		}, (error, res) => {
+		if (getState().requests.search) {
+			getState().requests.search.forEach(req => req.req.destroy());
+		}
+		dispatch(actions.clearSearchRequests());
+		const searchRequest = request({ url, body, json: true, method: 'post' }, (error, res) => {
 			if (error) return console.error(error);
 			if (res.statusCode === 400) return sweetAlert(res.body, null, 'error');
 			return dispatch(actions.populateQuestionSetList(res.body));
