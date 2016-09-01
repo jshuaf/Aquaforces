89e99568234fa3278274de891329ff825c623310
Joshua <joshuacfang@gmail.com>
2016-09-01 10:28:31 -0400

    clean up api file searching

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
-/* global generateID:true dbcs:true config:true*/
+/* global dbcs:true */
+
+const fs = require('fs');
+const responses = {};
+fs.readdirSync('./api').filter(file => file !== 'index.js').forEach((path) => {
+	/* eslint-disable global-require */
+	responses[path.substr(0, path.length - 3)] = require(`./${path}`);
+	/* eslint-enable global-require */
+});
 
 module.exports = function (req, res) {
 	res.badRequest = (message) => {
@@ -7,12 +15,11 @@ module.exports = function (req, res) {
 	};
 	if (req.params.path === 'authenticate') {
 		res.send(req.user ? JSON.stringify(req.user.personalInfo) : '');
+	} else if (Object.keys(responses).indexOf(req.params.path) >= 0) {
+		responses[req.params.path](req, res);
 	} else {
-		/* eslint-disable global-require */
-		require(`./${req.params.path}.js`)(req, res);
-		/* eslint-enable global-require */
+		res.writeHead(404);
+		return res.end('Error: The API feature requested has not been implemented.');
 	}
-	// res.writeHead(404);
-	// return res.end('Error: The API feature requested has not been implemented.');
 };
 
