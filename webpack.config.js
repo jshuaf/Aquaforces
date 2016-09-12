809476d7fd35b2afc949dec2f40a4a128408b619
Joshua <joshuacfang@gmail.com>
2016-09-12 06:41:06 -0400

    remove old wepback bundles

--- webpack.config.js
+++ webpack.config.js
@@ -49,9 +49,18 @@ module.exports = {
 		}),
 		function () {
 			this.plugin('done', (stats) => {
-				fs.writeFileSync(
-				path.join(__dirname, 'stats.json'),
-				JSON.stringify(stats.toJson()));
+				fs.writeFileSync(path.join(__dirname, 'stats.json'), JSON.stringify(stats.toJson()));
+				const chunkNames = require('./stats').assetsByChunkName
+				const recent = [].concat.apply([], Object.keys(chunkNames)
+					.map(chunkName => chunkNames[chunkName]))
+				const files = fs.readdirSync('./http')
+			  files.filter((name) => {
+					return /\w+\.bundle\.js\.map/.test(name) || /\w+\.bundle\.js/.test(name)
+				}).forEach((file) => {
+					if (recent.indexOf(file) < 0) {
+						fs.unlink(`./http/${file}`);
+					}
+				});
 			});
 		},
 	],
